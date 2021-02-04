c40bad151406e37121dfc362decc900aefcc2b8b
Aleksandra Kamińska <dwantula@gmail.com>
2021-02-04 13:54:12 +0100

    corrected layout page

--- src/shared/components/Logo/Logo.jsx
+++ src/shared/components/Logo/Logo.jsx
@@ -5,11 +5,11 @@ import './styles.scss';
 function Logo() {
   return (
     <div className="logo">
-      <div className="logo__column">
-        <p className="logo__text">ABC</p>
-        <div className="logo__icon" />
+      <div className="logo__abc-row">
+        <p className="logo__abc-text">ABC</p>
+        <div className="logo__icon"></div>
       </div>
-      <div className="logo__text-row">
+      <div className="logo__cams-row">
         <span>C</span>
         <span>A</span>
         <span>M</span>
