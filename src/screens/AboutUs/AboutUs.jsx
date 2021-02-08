c703ed83859eb4be190407251494660e624db3fd
Aleksandra Kamińska <dwantula@gmail.com>
2021-02-08 09:14:00 +0100

    implemented layout styles

--- src/screens/AboutUs/AboutUs.jsx
+++ src/screens/AboutUs/AboutUs.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import classNames from 'classnames';
+
 import Card from 'shared/components/Card/Card';
 
 import './styles.scss';
@@ -26,15 +26,17 @@ const cards = [
 ];
 
 function AboutUs() {
-  const classValue = classNames({ card: true, about__card: true });
-
   return (
     <div className="about">
       {cards.map(({ img, title, text }) => (
-        <Card key={title} className={classValue}>
-          <img className="about__image" alt="gadgets" src={img} />
-          <h3 className="about__title">{title}</h3>
-          <p className="about__text">{text}</p>
+        <Card key={img} cardClass={true}>
+          <div className="about__image-container">
+            <img className="about__image" alt="gadgets" src={img} />
+          </div>
+          <div className="about__text-container">
+            <h3 className="about__title">{title}</h3>
+            <p className="about__text">{text}</p>
+          </div>
         </Card>
       ))}
     </div>
